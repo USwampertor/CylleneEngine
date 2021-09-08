7107deddbbeabf12eb0d2c744281ff3fd1c1f0b3
USwampertor <idv16c.mmillan@uartesdigitales.edu.mx>
2021-09-08 17:24:47 -0500

    * Finishing up with quaternions + Euler + cyEulerHelpers.cpp + cyEulerHelpers.h + Added helpers to make rotation and creation of quaternions from eulers easier

--- cyEngine/cyUtilities/include/cyQuaternion.h
+++ cyEngine/cyUtilities/include/cyQuaternion.h
@@ -21,44 +21,353 @@ namespace CYLLENE_SDK {
   class Matrix3x3;
   class Matrix4x4;
 
+  struct CY_UTILITY_EXPORT Euler {
+  public:
+
+    Euler() = default;
+
+    Euler(const float& nx, const float& ny, const float& nz);
+
+    Euler(const Vector3f& vector);
+
+    Euler(const Vector4f& vector);
+
+  public:
+    float x;
+    float y;
+    float z;
+  };
+
+
   class CY_UTILITY_EXPORT Quaternion {
     
   public:
     
+    // Default constructor
     Quaternion() = default;
 
+    ~Quaternion() = default;
+
+    /**
+     * @brief Constructs without any transformation, passing values as is
+     * @param float nx
+     * @param float ny
+     * @param float nz
+     *
+     */
     Quaternion(const float& nx, const float& ny, const float& nz, const float& nw)
       : x(nx),
         y(ny),
         z(nz),
         w(nw) {}
 
+    /**
+     * @brief Constructs without any transformation, passing values as is
+     * @param Vector3f vector to add
+     * This Constructor fills the w value with 0
+     *
+     */
     Quaternion(const Vector3f& other);
 
+    /**
+     * @brief Constructs without any transformation, passing values as is
+     * @param Vector3f vector to add
+     * @param float the w part of the quaternion
+     *
+     */
     Quaternion(const Vector3f& other, const float& nw);
 
+    /**
+     * @brief Constructs without any transformation, passing values as is
+     * @param Vector4f The x y z w components
+     *
+     */
     Quaternion(const Vector4f other);
 
+    Quaternion(const Euler& euler, const int32& order);
+
+    /**
+     * @brief [] operator overload, will return the value at a given position
+     * @param index of the object we wanna get
+     * @return value at given index
+     *
+     */
+    float&
+    operator[](uint32 index);
+
+    /**
+     * @brief const [] operator overload, will return the value at a given position
+     * @param index of the object we wanna get
+     * @return value at given index
+     *
+     */
+    float
+    operator[](uint32 index) const;
+
+    /**
+     * @brief + operator overload
+     * @param b the other matrix to add
+     * @return a vector sum of *this and v
+     *
+     */
+    Quaternion
+    operator+(const Quaternion& other) const;
+  
+    /**
+     * @brief - operator overload
+     * @param b the other matrix to subtract
+     * @return a vector difference of *this and v
+     *
+     */
+    Quaternion
+    operator-(const Quaternion& other) const;
+  
+    /**
+     * @brief * operator overload
+     * @param b the other matrix to multiply
+     * @return a vector multiplication of *this times v
+     *
+     */
     Quaternion
     operator*(const Quaternion& other) const;
+  
+    /**
+     * @brief / operator overload with quaternions
+     * @param the quaternion to divide
+     * @return a quaternion divided by another quaternion
+     *
+     */
+    Quaternion
+    operator/(const Quaternion& other) const;
 
-    Vector4f 
-    toVector4();
+    /**
+     * @brief / operator overload
+     * @param the scale to divide
+     * @return a quaternion divided by scale
+     *
+     */
+    Quaternion
+    operator/(const float& scale) const;
+
+    /**
+     * @brief / operator overload
+     * @param float the scale to multiply
+     * @return a vector scaled by scale
+     *
+     */
+    Quaternion
+    operator*(const float& scale) const;
+  
+    /**
+     * @brief Operator = overload
+     * @param const Quaternion& other quaternion to evaluate
+     * @return Quaternion this
+     *
+     */
+    Quaternion&
+    operator=(const Quaternion& other);
+
+    /**
+     * @brief == operator overload
+     * @param the other vector to compare
+     * @return true if this is == to v components
+     *
+     */
+    bool
+    operator==(const Quaternion& other);
+
+    /**
+     * @brief != operator overload
+     * @param the other vector to compare
+     * @return true if this is != to v components
+     *
+     */
+    bool
+    operator!=(const Quaternion& other);
+
+    /**
+     * @brief < operator overload
+     * @param the other vector to compare
+     * @return true if this components are < to v components
+     *
+     */
+    bool
+    operator<(const Quaternion& other);
+
+    /**
+     * @brief > operator overload
+     * @param the other vector to compare
+     * @return true if this components are > to v components
+     *
+     */
+    bool
+    operator>(const Quaternion& other);
+
+    /**
+     * @brief <= operator overload
+     * @param the other vector to compare
+     * @return true if this components are <= to v components
+     *
+     */
+    bool
+    operator<=(const Quaternion& other);
+
+    /**
+     * @brief >= operator overload
+     * @param the other vector to compare
+     * @return true if this components are >= to v components
+     *
+     */
+    bool
+    operator>=(const Quaternion& other);
+  
+    /**
+     * @brief negates the whole quaternion
+     * @param 
+     * @return 
+     *
+     */
+    Quaternion
+    operator-() const;
+
+    /**
+     * @brief += operator overload
+     * @param the other vector to add
+     * @return *this + v components
+     *
+     */
+    Quaternion&
+    operator+=(const Quaternion& other);
+  
+    /**
+     * @brief -= operator overload
+     * @param the other vector to subtract
+     * @return *this - v components
+     *
+     */
+    Quaternion&
+    operator-=(const Quaternion& other);
+  
+    /**
+     * @brief *= operator overload
+     * @param the other vector to multiply
+     * @return *this * v components
+     *
+     */
+    Quaternion&
+    operator*=(const Quaternion& other);
+
+    /**
+     * @brief /= operator overload with quaternions
+     * @param the other quaternion to divide
+     * @return *this / q components
+     *
+     */
+    Quaternion&
+    operator/=(const Quaternion& other);
+
+    /**
+     * @brief *= operator overload
+     * @param the float to scale the quaternion
+     * @return *this * v components
+     *
+     */
+    Quaternion&
+    operator*=(float scale);
+  
+    /**
+     * @brief /= operator overload
+     * @param the float to scale the quaternion
+     * @return *this / v components
+     *
+     */
+    Quaternion&
+    operator/=(float scale);
+  
+    /**
+     * @brief | operator overload for dot product
+     * @param the other vector
+     * @return dot product
+     *
+     */
+    float
+    operator|(const Quaternion& v) const;
 
     const Vector3f&
     getVectorPart() const;
 
     const float&
     getScalarPart() const;
-
     const Matrix3x3&
     getRotationMatrix() const;
+
+    Vector4f 
+    toVector4() const;
 
     static const Vector3f
     transform(const Vector3f& vector, const Quaternion& quaternion);
 
     void
     setRotationMatrix(const Matrix3x3& m);
+
+    void
+    fromEuler(const Euler&, int32 order);
+
+    void
+    fromEuler(const Vector3f&, int32 order);
+
+    const Euler
+    toEuler();
+
+    void
+    setValues(const float& nx, const float& ny, const float& nz, const float& nw);
+
+    void 
+    setValues(const Vector3f& vector, const float& scalar);
+
+    float
+    magnitude() const;
+
+    float 
+    norm() const;
+
+    Quaternion
+    scaled(const float& scale) const;
+
+    void 
+    scale(const float& scale);
+
+    Quaternion
+    conjugated() const;
+
+    void
+    conjugate();
+
+    Quaternion
+    inversed() const;
+
+    void 
+    inverse();
+
+    Quaternion
+    united() const;
+
+    void
+    unit();
+
+    bool
+    isPure() const;
+
+    bool
+    isReal() const;
+
+
+    static const Quaternion
+    slerp(const Quaternion& q1, const Quaternion& q2, float t);
+
+    String
+    toString();
+
 
   public:
 
--- cyEngine/cyUtilities/source/cyQuaternion.cpp
+++ cyEngine/cyUtilities/source/cyQuaternion.cpp
@@ -9,11 +9,29 @@
 /*0***0***0***0***0***0***0***0***0***0***0***0***0***0***0***0*/
 #include "cyQuaternion.h"
 
+#include "cyEulerHelpers.h"
 #include "cyVector3f.h"
 #include "cyVector4f.h"
 #include "cyMatrix3x3.h"
+#include "cyMath.h"
 
 namespace CYLLENE_SDK {
+
+  Euler::Euler(const float& nx, const float& ny, const float& nz)
+    : x(nx),
+      y(ny),
+      z(nz) {}
+
+  Euler::Euler(const Vector4f& vector)
+    : x(vector.x),
+      y(vector.y),
+      z(vector.z) {}
+
+  Euler::Euler(const Vector4f& vector)
+    : x(vector.x),
+      y(vector.y),
+      z(vector.z) {}
+
   Quaternion::Quaternion(const Vector3f& other) 
   : x(other.x),
     y(other.y),
@@ -32,6 +50,44 @@ namespace CYLLENE_SDK {
       z(other.z),
       w(other.w) {}
 
+  Quaternion::Quaternion(const Euler& euler, const int32& order) {
+
+    EulerAngles ea;
+    
+    ea.x = euler.x;
+    ea.y = euler.y;
+    ea.z = euler.z;
+    ea.w = order;
+
+    Quat q = Eul_ToQuat(ea);
+
+    x = q.x;
+    y = q.y;
+    z = q.z;
+    w = q.w;
+  }
+
+  float&
+  Quaternion::operator[](uint32 index) {
+    return (&x)[index];
+  }
+
+  float
+  Quaternion::operator[](uint32 index) const {
+    return (&x)[index];
+  }
+
+  Quaternion
+  Quaternion::operator+(const Quaternion& other) const {
+    return Quaternion(x + other.x, y + other.y, z + other.z, w + other.w);
+  }
+  
+  Quaternion
+  Quaternion::operator-(const Quaternion& other) const {
+    return Quaternion(x - other.x, y - other.y, z - other.z, w - other.w);
+  }
+  
+
   Quaternion
   Quaternion::operator*(const Quaternion& other) const {
     return Quaternion(w* other.x + x * other.w + y * other.z - z * other.y,
@@ -40,17 +96,161 @@ namespace CYLLENE_SDK {
                       w* other.w - x * other.x - y * other.y - z * other.z);
   }
 
+  Quaternion
+  Quaternion::operator/(const Quaternion& other) const {
+    
+    return *this * other.inversed();
+    // float divider = Math::sqr(other.x) + 
+    //                 Math::sqr(other.y) + 
+    //                 Math::sqr(other.z) +
+    //                 Math::sqr(other.w);
+    // 
+    // return Quaternion((other.w * x - other.x * w - other.y * z + other.z * y) / divider,
+    //                   (other.w * y + other.x * z - other.y * w - other.z * x) / divider,
+    //                   (other.w * z - other.x * y + other.y * x - other.z * w) / divider,
+    //                   (other.w * w + other.x * x + other.y * y + other.z * z) / divider);
+  }
+
+  Quaternion
+  Quaternion::operator/(const float& scale) const {
+    return Quaternion(x / scale, y / scale, z / scale, w / scale);
+  }
+
+  Quaternion
+  Quaternion::operator*(const float& scale) const {
+    return Quaternion(x * scale, y * scale, z * scale, w * scale);
+  }
+
+  Quaternion&
+  Quaternion::operator=(const Quaternion& other) {
+    x = other.x;
+    y = other.y;
+    z = other.z;
+
+    w = other.w;
+
+    return *this;
+  }
+
+  bool
+  Quaternion::operator==(const Quaternion& other) {
+    return x == other.x && y == other.y && z == other.z && w == other.w;
+  }
+
+  bool
+  Quaternion::operator!=(const Quaternion& other) {
+    return x != other.x && y != other.y && z != other.z && w != other.w;
+  }
+
+  bool
+  Quaternion::operator<(const Quaternion& other) {
+    return x < other.x && y < other.y && z < other.z && w < other.w;
+  }
+
+  bool
+  Quaternion::operator>(const Quaternion& other) {
+    return x > other.x && y > other.y && z > other.z && w > other.w;
+  }
+
+  Quaternion
+  Quaternion::operator-() const {
+    return Quaternion(-x, -y, -z, -w);
+  }
+
+  bool
+  Quaternion::operator<=(const Quaternion& other) {
+    return x <= other.x && y <= other.y && z <= other.z && w <= other.w;
+  }
+
+  bool
+  Quaternion::operator>=(const Quaternion& other) {
+    return x >= other.x && y >= other.y && z >= other.z && w >= other.w;
+  }
+
+  Quaternion&
+  Quaternion::operator+=(const Quaternion& other) {
+    x += other.x;
+    y += other.y;
+    z += other.z;
+    w += other.w;
+    return *this;
+  }
+  
+  Quaternion&
+  Quaternion::operator-=(const Quaternion& other) {
+    x -= other.x;
+    y -= other.y;
+    z -= other.z;
+    w -= other.w;
+    return *this;
+  }
+  
+  Quaternion&
+  Quaternion::operator*=(const Quaternion& other) {
+    
+    Quaternion newQuat;
+    newQuat.w = w * other.w - x * other.x - y * other.y - z * other.z;
+    newQuat.x = w * other.x + x * other.w + y * other.z - z * other.y;
+    newQuat.y = w * other.y - x * other.z + y * other.w + z * other.x;
+    newQuat.z = w * other.z + x * other.y - y * other.x + z * other.w;
+
+    *this = newQuat;
+
+    return *this;
+  }
+  
+  Quaternion&
+  Quaternion::operator/=(const Quaternion& other) {
+
+
+    *this = *this * other.inversed();
+    // float divider = Math::sqr(other.x) +
+    //                 Math::sqr(other.y) +
+    //                 Math::sqr(other.z) +
+    //                 Math::sqr(other.w);
+    // 
+    // *this = Quaternion((other.w * x - other.x * w - other.y * z + other.z * y) / divider,
+    //                    (other.w * y + other.x * z - other.y * w - other.z * x) / divider,
+    //                    (other.w * z - other.x * y + other.y * x - other.z * w) / divider,
+    //                    (other.w * w + other.x * x + other.y * y + other.z * z) / divider);
+    return *this;
+  }
+
+  Quaternion&
+  Quaternion::operator*=(float scale) {
+    x *= scale;
+    y *= scale;
+    z *= scale;
+    w *= scale;
+    return *this;
+  }
+
+  Quaternion&
+  Quaternion::operator/=(float scale) {
+    x /= scale;
+    y /= scale;
+    z /= scale;
+    w /= scale;
+    return *this;
+  }
+
+  float
+  Quaternion::operator|(const Quaternion& v) const {
+    return x * v.x + y * v.y + z * v.z + w * v.w;
+  }
+
   const Vector3f
   Quaternion::transform(const Vector3f& vector, const Quaternion& quaternion) {
     auto tmp = quaternion.getVectorPart();
     float b2 = tmp.sqrMagnitude();
-    return (vector * (quaternion.w * quaternion.w - b2) + tmp * (Vector3f::dot(vector, tmp) * 2.0f)
-      + Vector3f::cross(tmp, vector) * (quaternion.w * 2.0f));
+    return (vector * (quaternion.w * quaternion.w - b2) + 
+            tmp * (Vector3f::dot(vector, tmp) * 2.0f) + 
+            Vector3f::cross(tmp, vector) * (quaternion.w * 2.0f));
   }
 
   Vector4f 
-  Quaternion::toVector4() {
-  
+  Quaternion::toVector4() const {
+    return Vector4f(x, y, z, w);
   }
 
   const Vector3f&
@@ -84,7 +284,58 @@ namespace CYLLENE_SDK {
 
   void
   Quaternion::setRotationMatrix(const Matrix3x3& m) {
+    float sum = m._m.m00 + m._m.m11 + m._m.m22;
 
+  }
+
+  float
+  Quaternion::norm() const {
+    return Math::sqr(x) + Math::sqr(y) + Math::sqr(z) + Math::sqr(w);
+  }
+
+  float
+  Quaternion::magnitude() const {
+    return Math::sqrt(this->norm());
+  }
+
+  Quaternion
+  Quaternion::scaled(const float& scale) const {
+    return Quaternion(x * scale, y * scale, z * scale, w * scale);
+  }
+
+  void
+  Quaternion::scale(const float& scale) {
+    *this = scaled(scale);
+  }
+
+  Quaternion
+  Quaternion::inversed() const {
+    return conjugated().scaled(1/norm());
+  }
+
+  void
+  Quaternion::inverse() {
+    *this = inversed();
+  }
+
+  Quaternion
+  Quaternion::conjugated() const {
+    return Quaternion(-x, -y, -z, w);
+  }
+
+  void
+  Quaternion::conjugate() {
+    *this = conjugated();
+  }
+
+  Quaternion
+  Quaternion::united() const {
+    return scaled(Math::invSqrt(norm()));
+  }
+
+  void
+  Quaternion::unit() {
+    *this = united();
   }
 
 }
