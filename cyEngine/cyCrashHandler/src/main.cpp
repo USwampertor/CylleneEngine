4f79313937d4433668250695b43f4ce10b1743f8
USwampertor <idv16c.mmillan@uartesdigitales.edu.mx>
2021-10-27 11:15:59 -0500

    * a

--- cyEngine/cyCrashHandler/src/main.cpp
+++ cyEngine/cyCrashHandler/src/main.cpp
@@ -1,7 +1,7 @@
 
 #include "cyCrashHandlerPrerequisites.h"
 #include "cyGraphicsUI.h"
-
+#include "cyUtilities.h"
 #define DIRECTX11 1;
 
 #if CY_PLATFORM == CY_PLATFORM_WIN32
@@ -15,16 +15,26 @@
 using namespace CYLLENE_SDK;
 
 int main(int argc, char* argv[]) {
-  GraphicsUI* ui;
+
+  try
+  {
 #if CY_PLATFORM == CY_PLATFORM_WIN32
 # ifdef DIRECTX11
-  ui = new Directx11UI();
+  GraphicsUI::startUp<Directx11UI>();
   ImGuiIO a;
-  ui->init(a, nullptr);
+    if (!GraphicsUI::instance().init(a, nullptr)) {
+      String error("Could not generate Instance");
+      Utils::ThrowException(error);
+    }
 # endif // DIRECTX11
 #elif CY_PLATFORM == CY_PLATFORM_LINUX
 
 #endif
+  }
+  catch (const std::exception& e)
+  {
+
+  }
 
   return 0;
   
