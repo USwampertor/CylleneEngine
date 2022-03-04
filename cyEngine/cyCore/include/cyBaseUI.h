0d37df324aef3d58d4f5612abcdea5eeffa859d2
Marco <ac13300217@gmail.com>
2022-03-04 06:27:31 -0600

    + Adding BaseUI

---
+++ cyEngine/cyCore/include/cyBaseUI.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "cyCorePrerequisites.h"
+#include <imgui/imgui.h>
+#include <cyModule.h>
+
+namespace CYLLENE_SDK {
+class CY_CORE_EXPORT BaseUI : Module<BaseUI>
+{
+public:
+  BaseUI() = default;
+
+  ~BaseUI() = default;
+
+  bool
+  registerMenu(const String& tabName);
+
+  bool
+  registerSubMenu(const String& father, const String& child);
+
+  bool
+  unregisterMenu(const String& tabName);
+
+  bool
+  unregisterSubMenu(const String& father, const String& child);
+
+  Vector<Vector<String>> menus;
+
+  void
+  displayUI();
+
+  void
+  newFrame();
+
+  void
+  endFrame();
+
+
+};
+}
