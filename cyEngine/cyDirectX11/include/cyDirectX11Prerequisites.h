eb4265d6497a6f715784623569c692150e186f22
USwampertor <mmillan@uartesdigitales.edu.mx>
2025-04-29 13:14:52 -0600

    * GraphicsDX11API

--- cyEngine/cyDirectX11/include/cyDirectX11Prerequisites.h
+++ cyEngine/cyDirectX11/include/cyDirectX11Prerequisites.h
@@ -1,1 +1,33 @@
 #pragma once
+
+#include <cyUtilitiesPrerequisites.h>
+
+#if CY_PLATFORM == CY_PLATFORM_WIN32
+# if CY_COMPILER == CY_COMPILER_MSVC
+#   if defined( CY_STATIC_LIB )
+#     define CY_DX11_EXPORT
+#   else
+#     if defined( CY_DX11_EXPORTS )
+#       define CY_DX11_EXPORT __declspec( dllexport )
+#     else
+#       define CY_DX11_EXPORT __declspec( dllimport )
+#     endif
+#   endif
+# else //Any other compiler
+#   if defined( CY_STATIC_LIB )
+#     define CY_DX11_EXPORT
+#   else
+#     if defined( CY_DX11_EXPORTS )
+#       define CY_DX11_EXPORT __attribute__ ((dllexport))
+#     else
+#       define CY_DX11_EXPORT __attribute__ ((dllimport))
+#     endif
+#   endif
+# endif
+# define CY_DX11_HIDDEN
+#else //Linux/Mac Settings
+# define CY_DX11_EXPORT __attribute__ ((visibility ("default")))
+# define CY_DX11_HIDDEN __attribute__ ((visibility ("hidden")))
+#endif
+
+
--- cyEngine/cyDirectX11/include/cyGraphicsDX11API.h
+++ cyEngine/cyDirectX11/include/cyGraphicsDX11API.h
@@ -1,5 +1,16 @@
 #pragma once
-class GraphicsDX11API
+#include "cyDirectX11Prerequisites.h"
+#include <cyGraphicsAPI.h>
+
+namespace CYLLENE_SDK {
+
+class CY_DX11_EXPORT GraphicsDX11API : public GraphicsAPI
 {
+public:
+  GraphicsDX11API() = default;
+  ~GraphicsDX11API() override;
+
 };
+
+}
 
