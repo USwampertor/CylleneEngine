ec7f83473f2013b1e941544d75ebcdcc12df57a5
USwampertor <mmillan@uartesdigitales.edu.mx>
2025-04-30 07:26:12 -0600

    * Adding missing functions to Rect

--- cyEngine/cyMath/source/cySphere.cpp
+++ cyEngine/cyMath/source/cySphere.cpp
@@ -7,15 +7,15 @@ namespace CYLLENE_SDK {
 
   String
   Sphere::toString() {
-    return Utils::format("A: %s, B: %s, Height: %2.f");
+    return Utils::format("Center: %s, radius: %2.f", m_center.toString().c_str(), m_radius);
   }
 
   bool
   Sphere::intersects(const Primitive& other) {
     const PRIMITIVE_TYPE::E type = other.getType();
     if (type == +PRIMITIVE_TYPE::E::SPHERE) {
-      const Sphere& otherCapsule = static_cast<const Sphere&>(other);
-
+      const Sphere& otherSphere = static_cast<const Sphere&>(other);
+      
     }
 
     return false;
