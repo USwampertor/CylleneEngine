e3e3a0bc66b1ec85c267f178f9001608155fed3c
USwampertor <mmillan@uartesdigitales.edu.mx>
2025-04-30 09:05:12 -0600

    * Fixing viewport

--- cyEngine/cyGraphics/source/cyGViewport.cpp
+++ cyEngine/cyGraphics/source/cyGViewport.cpp
@@ -5,10 +5,10 @@ namespace CYLLENE_SDK
 
 void
 GViewport::setViewport(int32 x, int32 y, int32 width, int32 height) {
-  // m_rect.x = x;
-  // m_rect.y = y;
-  // m_rect.width = width;
-  // m_rect.height = height;
+  m_rect.x = x;
+  m_rect.y = y;
+  m_rect.width = width;
+  m_rect.height = height;
 }
 
 }
