77fda0904252b481d2f8bbf53a5da77a9f60c686
USwampertor <mmillan@uartesdigitales.edu.mx>
2025-05-18 22:07:55 -0600

    * Fixing the way shader resourceview works

--- cyEngine/cyGraphics/source/cyGraphicsAPI.cpp
+++ cyEngine/cyGraphics/source/cyGraphicsAPI.cpp
@@ -1,1 +1,24 @@
 #include "cyGraphicsAPI.h"
+
+namespace CYLLENE_SDK
+{
+  
+void
+GraphicsAPI::addToRenderPool(uint32 index) {
+
+}
+
+void
+GraphicsAPI::removeFromRenderPool(uint32 index) {
+
+}
+
+void
+GraphicsAPI::registerToRenderPool(uint32 index) {
+
+}
+
+
+
+
+}
