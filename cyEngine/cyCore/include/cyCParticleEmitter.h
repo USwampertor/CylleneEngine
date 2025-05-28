cceabb52205c7b04f676ae7b0acb1dac0c388253
USwampertor <mmillan@uartesdigitales.edu.mx>
2025-05-28 14:23:44 -0600

    + Particle Emitter * Component enums * Added to core project

---
+++ cyEngine/cyCore/include/cyCParticleEmitter.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "cyCorePrerequisites.h"
+
+#include "cyCComponent.h"
+#include <cyVector3f.h>
+#include "cyRMesh.h"
+
+namespace CYLLENE_SDK {
+
+class CParticleEmitter : public CComponent
+{
+  CParticleEmitter() : CComponent(CParticleEmitter::staticType()) {}
+
+  static COMPONENT_TYPE::E staticType() { return COMPONENT_TYPE::E::ePARTICLEEMITTER; }
+
+  virtual const String 
+  toString() override {
+    String toReturn;
+
+    return toReturn;
+  }
+
+  float m_amount;
+
+  float m_unitLifeTime;
+
+  bool m_loop;
+  
+  bool m_usesGravity;
+
+  float m_duration;
+
+  Vector3f m_speed;
+
+  SPtr<RMesh> m_mesh;
+
+};
+
+}
+
