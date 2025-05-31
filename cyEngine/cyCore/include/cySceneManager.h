347701657984aa0f1e6fca1225995838be5d0d4c
USwampertor <mmillan@uartesdigitales.edu.mx>
2025-05-31 12:28:01 -0600

    + Adding ResourceManager * Adding infrastructure to Scenes

---
+++ cyEngine/cyCore/include/cySceneManager.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "cyCorePrerequisites.h"
+
+#include "cyScene.h"
+#include "cySceneGraph.h"
+#include "cyBBeing.h"
+
+namespace CYLLENE_SDK {
+
+class SceneManager
+{
+public:
+  template <typename T,
+  typename = std::enable_if_t<std::is_base_of<BBeing, T>::value>>
+    void destroyObject(SPtr<T> toDelete) {
+    toDelete->m_markedToDestroy = true;
+    m_activeScene->m_toRemove.push_back(toDelete);
+
+  }
+
+  template <typename T,
+    typename = std::enable_if_t<std::is_base_of<BBeing, T>::value>,
+    typename... Args>
+  SPtr<T> createObject(Args ... args)
+  {
+    SPtr<T> newBeing = makeSharedPtr<T>(std::forward<Args>(args)...);
+    newBeing->Initialize();
+    m_activeScene->m_beings.push_back(newBeing);
+    return newBeing;
+  }
+
+  template <typename T,
+    typename = std::enable_if_t<std::is_base_of<BBeing, T>::value>>
+  SPtr<T> findObject(const String& toFind) {
+    int i = 0;
+    for (const SPtr<BBeing>& e : m_activeScene->m_beings)
+    {
+      if (e->GetName() == toFind && !e->m_markedToDestroy)
+      {
+        return std::static_pointer_cast<T>(e);
+      }
+      ++i;
+    }
+    return nullptr;
+  }
+
+  SPtr<Scene> createScene(const String& newSceneName);
+
+  SPtr<Scene> loadScene(const String& newSceneName);
+
+  SPtr<Scene> unloadScene(const String& sceneToUnload);
+
+  SPtr<Scene> findScene(const String& sceneToFind);
+
+  bool changeScene(const String& sceneToLoad);
+
+  void saveScene();
+
+  void update(const float& delta);
+
+  // void UpdateRender(RenderWindow& w);
+
+  SPtr<Scene> getActiveScene();
+
+  JSONDocument serialize(const Vector<String>& names);
+
+  void deserialize(const JSONValue& sceneArray);
+
+  SPtr<Scene> m_activeScene;
+
+  Vector<SPtr<Scene>> m_scenes;
+
+  bool m_isDebug = false;
+};
+
+}
+
--- cyEngine/cyCore/src/cyScene.cpp
+++ cyEngine/cyCore/src/cyScene.cpp
@@ -1,9 +1,31 @@
 #include "cyScene.h"
 
 namespace CYLLENE_SDK {
+  void
+  Scene::init() {
 
-Scene::Scene(const String& name) : m_name(name) {
-  
-}
+  }
+
+  void
+  Scene::onSceneLoaded() {
+
+  }
+
+  void
+  Scene::onSceneUnloaded() {
+
+  }
+
+  JSONDocument
+  Scene::serialize() {
+    JSONDocument document;
+
+    return document;
+  }
+
+  void
+  Scene::deserialize(const JSONValue& sceneData) {
+
+  }
 
 }
