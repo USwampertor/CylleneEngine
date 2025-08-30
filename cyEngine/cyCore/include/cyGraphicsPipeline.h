580474805ebd6b46af38ed0326639eb04f2dcce4
USwampertor <mmillan@uartesdigitales.edu.mx>
2025-08-29 23:21:50 -0600

    * Updating GraphicPass

--- cyEngine/cyCore/include/cyGraphicsPipeline.h
+++ cyEngine/cyCore/include/cyGraphicsPipeline.h
@@ -17,8 +17,21 @@ class GraphicsPipeline
 {
 public:
 
+  void
+  addPass(SPtr<GraphicPass> newPass) {
+    m_passes.push_back(newPass);
+  }
+
   Vector<SPtr<GraphicPass>> 
   getPasses() { return m_passes; }
+
+  SPtr<GraphicPass>
+  getAt(int32 index) {
+    if (index < m_passes.size() && index >= 0) {
+      return m_passes[index];
+    }
+    return nullptr;
+  }
 
 private:
 
