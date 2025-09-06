d850f8d808544d8c9d6fe5f516148e75f7dc5441
USwampertor <mmillan@uartesdigitales.edu.mx>
2025-09-06 10:39:39 -0600

    + GMaterial helper to get reflection material information from the shader

---
+++ cyEngine/cyGraphics/include/cyGMaterial.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "cyGraphicsPrerequisites.h"
+#include "cyGGraphic.h"
+#include "cyGShader.h"
+
+#include <cyRMaterial.h>
+
+namespace CYLLENE_SDK {
+
+class GMaterial : public GGraphic
+{
+public:
+  GMaterial() = default;
+  ~GMaterial() = default;
+
+  bool initialize();
+
+  GShader* GetShader() const { return m_shader.get(); }
+
+  const MaterialValue* GetDefaultValue(const std::string& name) const;
+
+  void SetDefaultValue(const std::string& name, const MaterialValue& value);
+
+private:
+  
+  WPtr<RMaterial> m_material;
+  WPtr<GShader> m_shader;
+  std::unordered_map<std::string, MaterialValue> m_defaultValues;
+
+
+};
+
+}
+
