ff7499fb93766160551ede8290ae3735a5e712c9
USwampertor <mmillan@uartesdigitales.edu.mx>
2025-09-06 09:03:31 -0600

    * Refactorization of material

--- cyEngine/cyCore/src/cyRMaterial.cpp
+++ cyEngine/cyCore/src/cyRMaterial.cpp
@@ -12,22 +12,5 @@ RMaterial::setData(void* data) {
 
 }
 
-void
-RMaterial::setShader(const SPtr<RShader>& newShader) {
-  if (newShader == nullptr) {
-    return;
-  }
-
-  m_shader = newShader;
-}
-
-void
-RMaterial::setTexture(TEXTURE_CHANNEL::E name, const SPtr<RTexture>& texture) {
-  if (texture == nullptr) {
-    return;
-  }
-  m_textures[name] = texture;
-}
-
 
 }
