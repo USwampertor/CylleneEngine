9e67927e48aef51ed1147d9b4339487894343092
USwampertor <mmillan@uartesdigitales.edu.mx>
2025-11-08 08:56:02 -0600

    * reorder

--- cyEngine/cyCore/include/cyBBeing.h
+++ cyEngine/cyCore/include/cyBBeing.h
@@ -106,8 +106,8 @@ public:
     if (m_components.find(type) == m_components.end()) {
       // m_components.insert(Utils::makePair(type, makeSharedPtr<T>(args ...)));
       SPtr<T> newComponent = makeSharedPtr<T>(std::forward<Args>(args)...);
-      m_components.at(type)->m_onCreate.invoke();
       m_components.try_emplace(type, newComponent);
+      m_components.at(type)->m_onCreate.invoke();
       m_components.at(type)->setOwner(m_self);
       m_components.at(type)->m_onInit.invoke();
     }
