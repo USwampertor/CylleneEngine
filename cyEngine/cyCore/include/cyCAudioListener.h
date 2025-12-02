fd30727a898323fcfaef842a4ac86a2a46ec7351
USwampertor <mmillan@uartesdigitales.edu.mx>
2025-12-01 22:34:52 -0600

    * Continuing with the Audio API

--- cyEngine/cyCore/include/cyCAudioListener.h
+++ cyEngine/cyCore/include/cyCAudioListener.h
@@ -1,5 +1,56 @@
 #pragma once
-class CAudioListener
-{
+#include "cyCorePrerequisites.h"
+#include "cyAudioBackend.h"
+#include "cyCComponent.h"
+
+#include <cyVector3f.h>
+
+namespace CYLLENE_SDK {
+
+/*
+ *  @class  CAudioListener
+ *  @brief  Represents the global OpenAL listener state.
+ */
+class CAudioListener : public CComponent {
+public:
+
+  CAudioListener() : CComponent(CAudioListener::staticType()) {
+    AudioBackend::ensureOpenAL();
+    setPosition(m_position);
+    setVelocity(m_velocity);
+    setOrientation(m_forward, m_up);
+    setGain(m_gain);
+  }
+
+  static COMPONENT_TYPE::E staticType() { return COMPONENT_TYPE::E::eAUDIOLISTENER; }
+
+  virtual const String
+  toString() override {
+    return Utils::format("position: %s", m_position.toString());
+  }
+
+  ~CAudioListener();
+
+  void
+  setPosition(const Vector3f& position);
+
+  void
+  setVelocity(const Vector3f& velocity);
+
+  void
+  setOrientation(const Vector3f& forward, const Vector3f& up);
+
+  void
+  setGain(float gain);
+
+private:
+
+  Vector3f m_position = Vector3f::ZERO;
+  Vector3f m_velocity = Vector3f::ZERO;
+  Vector3f m_forward = Vector3f::FRONT;
+  Vector3f m_up = Vector3f::UP;
+  float m_gain = 1.0f;
 };
 
+
+}
--- cyEngine/cyCore/include/cyCAudioSource.h
+++ cyEngine/cyCore/include/cyCAudioSource.h
@@ -1,10 +1,95 @@
 #pragma once
 #include "cyCorePrerequisites.h"
+#include "cyRAudio.h"
+#include "cyCComponent.h"
+#include "cyAudioBackend.h"
 
 namespace CYLLENE_SDK {
 
-class CAudioSource
-{
+/*
+ *	@class	CAudioSource
+ *	@brief	An audio source that holds an audio clip to be played.
+ *
+ */
+class CAudioSource : public CComponent {
+public: 
+
+  CAudioSource() : CComponent(CAudioSource::staticType()) {
+    AudioBackend::ensureOpenAL();
+  }
+  
+  ~CAudioSource();
+
+  static COMPONENT_TYPE::E staticType() { return COMPONENT_TYPE::E::eAUDIOSOURCE; }
+
+  virtual const String
+  toString() override {
+    return Utils::format("%s is %s", (m_clip.lock() ? 
+                                      m_clip.lock()->getName().c_str() : 
+                                      "audio source without clip"), 
+                                     (m_isPlaying ?
+                                      "is playing" :
+                                       "is not playing"));
+  }
+
+  void
+  play();
+
+  void
+  playDelayed(float delay);
+
+  void
+  stop();
+
+  void
+  pause();
+
+  void
+  playOnce(SPtr<RAudio> audio);
+
+  void
+  setClip(WPtr<RAudio> clip) {
+    m_clip = clip;
+  }
+
+  WPtr<RAudio>
+  getClip() {
+    return m_clip;
+  }
+
+  bool
+  isPlaying() const {
+    return m_isPlaying;
+  }
+
+public:
+
+  float m_volume = 1.0f;
+
+  bool m_loop = false;
+
+  bool m_spatialize = false;
+
+  int m_priority = 128;
+
+  bool m_isPlaying = false;
+
+  bool m_mute = false;
+
+  bool m_playOnAwake = false;
+
+  bool m_bypassEffects = false;
+
+  float m_pitch = 1.0f;
+
+  WPtr<RAudio> m_clip;
+
+private:
+
+  unsigned int m_sourceId = 0;
+
+  unsigned int m_bufferId = 0;
+
 };
 
 }
